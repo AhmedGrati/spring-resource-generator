3532ff6bf08835cdfa249180ce15e6ebf5d871c7
AhmedGrati <ahmedgrati1999@gmail.com>
2021-06-17 13:16:01 +0100

    fix additional help message

--- src/main/java/helpers/AdditionalHelpMessage.java
+++ src/main/java/helpers/AdditionalHelpMessage.java
@@ -11,37 +11,6 @@ public class AdditionalHelpMessage {
     private String keyOfHeader;
     private String keyOfDetails;
 
-    public String getHeaderOfAdditionalHelpMessage() {
-        return headerOfAdditionalHelpMessage;
-    }
-
-    public void setHeaderOfAdditionalHelpMessage(String headerOfAdditionalHelpMessage) {
-        this.headerOfAdditionalHelpMessage = headerOfAdditionalHelpMessage;
-    }
-
-    public Map<String, String> getAdditionalMessageHelp() {
-        return additionalMessageHelp;
-    }
-
-    public void setAdditionalMessageHelp(Map<String, String> additionalMessageHelp) {
-        this.additionalMessageHelp = additionalMessageHelp;
-    }
-
-    public String getKeyOfHeader() {
-        return keyOfHeader;
-    }
-
-    public void setKeyOfHeader(String keyOfHeader) {
-        this.keyOfHeader = keyOfHeader;
-    }
-
-    public String getKeyOfDetails() {
-        return keyOfDetails;
-    }
-
-    public void setKeyOfDetails(String keyOfDetails) {
-        this.keyOfDetails = keyOfDetails;
-    }
 
     public AdditionalHelpMessage(
             String headerOfAdditionalHelpMessage,

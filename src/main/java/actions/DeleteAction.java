0e2e915e6d60fc9ee38d468b84b2f145abf4186a
AhmedGrati <ahmedgrati1999@gmail.com>
2021-06-25 13:37:12 +0100

    fix delete action lint

--- src/main/java/actions/DeleteAction.java
+++ src/main/java/actions/DeleteAction.java
@@ -3,10 +3,12 @@ package actions;
 import schematics.Resource;
 
 import java.io.IOException;
+import java.net.UnknownServiceException;
 
 public class DeleteAction implements AbstractAction{
     @Override
     public void execute(Resource resource, String resourceName) throws IOException {
+        throw new UnknownServiceException("This Method Is Not Implemented Yet");
 
     }
 }

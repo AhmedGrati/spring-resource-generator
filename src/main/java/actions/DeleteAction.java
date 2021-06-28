11e072c3c8b6f76041668d41227e32456703f70a
AhmedGrati <ahmedgrati1999@gmail.com>
2021-06-28 09:15:02 +0100

    fix the checkstyle of DeleteAction.java class

--- src/main/java/actions/DeleteAction.java
+++ src/main/java/actions/DeleteAction.java
@@ -2,12 +2,16 @@ package actions;
 
 import schematics.Resource;
 
-import java.io.IOException;
 import java.net.UnknownServiceException;
 
 public class DeleteAction implements AbstractAction {
+    /**
+     * @param resource
+     * @param resourceName
+     * @throws Exception
+     */
     @Override
-    public void execute(Resource resource, String resourceName) throws Exception{
+    public void execute(final Resource resource, final String resourceName) throws Exception {
         throw new UnknownServiceException("This Method Is Not Implemented Yet");
 
     }

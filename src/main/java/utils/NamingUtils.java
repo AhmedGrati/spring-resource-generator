3c598af5b0c30d18e60386fde2c7e7e379ba9e6f
AhmedGrati <ahmedgrati1999@gmail.com>
2021-06-30 14:22:21 +0100

    fix checkstyle for NamingUtils.java class

--- src/main/java/utils/NamingUtils.java
+++ src/main/java/utils/NamingUtils.java
@@ -1,21 +1,37 @@
 package utils;
 
-import schematics.*;
 
-public class NamingUtils {
-    public static String getPackageNameFromResourceAndAPIType(Resource resource, APITypeValues apiTypeValues) {
-        if(resource instanceof Entity) {
+import schematics.Entity;
+import schematics.Repository;
+import schematics.Resource;
+import schematics.Service;
+import schematics.Controller;
+
+
+public final class NamingUtils {
+    private NamingUtils() {
+
+    }
+
+    /**
+     * Given a resource and API Type, it returns the name of the package.
+     * @param resource
+     * @param apiTypeValues
+     * @return String
+     */
+    public static String getPackageNameFromResourceAndAPIType(final Resource resource, final APITypeValues apiTypeValues) {
+        if (resource instanceof Entity) {
             return PackageNameConstants.ENTITIES_PACKAGE_NAME;
         }
-        if(resource instanceof Repository) {
+        if (resource instanceof Repository) {
             return PackageNameConstants.REPOSITORIES_PACKAGE_NAME;
         }
-        if(resource instanceof Service) {
+        if (resource instanceof Service) {
             return PackageNameConstants.SERVICES_PACKAGE_NAME;
         }
-        if(resource instanceof Controller){
+        if (resource instanceof Controller) {
 
-            if(apiTypeValues.getValue().equals(APITypeValues.REST.getValue())) {
+            if (apiTypeValues.getValue().equals(APITypeValues.REST.getValue())) {
                 return PackageNameConstants.CONTROLLER_PACKAGE_NAME;
             }
             return PackageNameConstants.RESOLVER_PACKAGE_NAME;

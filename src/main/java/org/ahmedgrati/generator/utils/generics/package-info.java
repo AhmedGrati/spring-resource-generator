e1662f1650af6e5332c9a42acf3bf32bc0fb27fe
AhmedGrati <ahmedgrati1999@gmail.com>
2021-07-25 09:21:13 +0100

    fix checkstyle error in org.ahmedgrati.generator.utils.generics.package-info.java

--- src/main/java/org/ahmedgrati/generator/utils/generics/package-info.java
+++ src/main/java/org/ahmedgrati/generator/utils/generics/package-info.java
@@ -3,8 +3,8 @@
  *     This package provides the generic types and functions
  * </p>
  * <p>
- *     The {@link org.ahmedgrati.generator.utils.generics.TriConsumer} TriConsumer is an extension for the BiConsumer, where we get 3 parameters
- *     instead of 2 in the case of BiConsumer.
+ *     The {@link org.ahmedgrati.generator.utils.generics.TriConsumer} TriConsumer is an extension for the BiConsumer,
+ *     where we get 3 parameters instead of 2 in the case of BiConsumer.
  * </p>
  */
 
